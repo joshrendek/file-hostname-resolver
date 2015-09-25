3cfe1ac1f61b66e2f5950557939a3b0e6b6c8e9d
Josh Rendek <josh@bluescripts.net>
2015-09-25 14:51:45 -0400

    Added RDNS

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Usage
 
+Two modes:
+
+RDNS lookup for IPs, IP lookup for Hostnames
+
 ```
 go install github.com/joshrendek/file-hostname-resolver
-cat list-of-hostnames.txt | file-hostname-resolver
+cat list-of-hostnames.txt | file-hostname-resolver -ip-lookup
+cat list-of-ips.txt | file-hostname-resolver -rdns
 ```
--- resolver.go
+++ resolver.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -10,10 +11,23 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	var ipLookup bool
+	var rdnsLookup bool
+	flag.BoolVar(&ipLookup, "ip-lookup", false, "")
+	flag.BoolVar(&rdnsLookup, "rdns", false, "")
+	flag.Parse()
 	for scanner.Scan() {
-		ips, _ := net.LookupIP(strings.TrimSpace(scanner.Text()))
-		for _, ip := range ips {
-			fmt.Println(ip)
+		if ipLookup {
+			data, _ := net.LookupIP(strings.TrimSpace(scanner.Text()))
+			for _, d := range data {
+				fmt.Println(d)
+			}
+		}
+		if rdnsLookup {
+			data, _ := net.LookupAddr(strings.TrimSpace(scanner.Text()))
+			for _, d := range data {
+				fmt.Println(d)
+			}
 		}
 	}
 }
